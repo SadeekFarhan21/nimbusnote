1a5c3a704bfcc86e6c8360cc83fce215126d230b
Farhan Sadeek <farhansadeek019@gmail.com>
2024-04-27 16:35:01 -0400

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
     const [data, setData] = useState({});
@@ -42,6 +43,7 @@ function App() {
 
     return (
         <div className="app">
+            <Analytics />
             <div className="search">
                 <input
                     value={location}
